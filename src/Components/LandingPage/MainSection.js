baf66e19222662dbf1bdcda3aa34fafe6d913faa
Gareth Ng <garethcng@gmail.com>
2021-04-26 21:35:11 -0400

    landing page arrow

--- src/Components/LandingPage/MainSection.js
+++ src/Components/LandingPage/MainSection.js
@@ -1,8 +1,15 @@
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import EditorPreview from './EditorPreview'
+import { useState, useEffect } from 'react'
 
 const MainSection = () => {
+    const [isArrowVisible, setIsArrowVisible] = useState(false)
+
+    useEffect(()=> {
+        setIsArrowVisible(true)
+    }, [])
+
     return(
         <Container>
             <ContentContainer>
@@ -12,12 +19,65 @@ const MainSection = () => {
                 <StyledLink to='/writing-app/signup'>
                     <GetStarted>Get started now</GetStarted>
                 </StyledLink>
+                <TryItAbsolute>
+                    <TryItRelative>
+                        <Arrow isArrowVisible={isArrowVisible} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16 15v4l8-8.035-8-7.965v4s-13.277 2.144-16 14c5.796-6.206 16-6 16-6z"/></Arrow>
+                        <TryItText isArrowVisible={isArrowVisible}>Try it out!</TryItText>
+                    </TryItRelative>
+                </TryItAbsolute>
             </ContentContainer>
         </Container>
     )
 }
 
 export default MainSection
+
+const Arrow = styled.svg`
+    transform: ${props=>props.isArrowVisible ? 'rotate(240deg)' : 'rotate(200deg)'};
+    width: 80px;
+    height: 80px;
+    opacity: ${props=>props.isArrowVisible ? 1 : 0};
+    transition: opacity 400ms ease-in-out, transform 400ms ease-in-out;
+    transform-origin: bottom left;
+    transition-delay: 300ms;
+    @media(max-width: 600px) {
+        transform: rotate(260deg);
+        width: 55px;
+        height: 55px;
+    }
+`
+
+const TryItText = styled.h3`
+    opacity: ${props=>props.isArrowVisible ? 1 : 0};
+    transition: opacity 400ms ease-in-out;
+    transition-delay: 800ms;
+    position: relative;
+    top: -10px;
+    left: 0px;
+    font-size: 1.25rem;
+    font-weight: 600;
+    @media(max-width: 600px) {
+        font-size: 1.125rem;
+        top: -15px;
+        left: 0px;
+    }
+`
+
+const TryItRelative = styled.div`
+    position: relative;
+    top: -100px;
+    @media(max-width: 600px) {
+        top: -80px;
+    }
+`
+
+const TryItAbsolute = styled.div`
+    position: absolute;
+    left: 70%;
+    @media(max-width: 400px) {
+        display: none;
+    }
+`
 
 const ContentContainer = styled.div`
     padding: 0 30px;
